f85771a863ade01e44bf872654cf0a7924d66401
lenorlevy <lenorlevy1@gmail.com>
2018-08-10 17:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver() {
+drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver() {
+drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+drivers.shift();
+}
+
+function appendDriver(name) {
+var newDrivers = [...drivers]
+newDrivers.push(name);
+return newDrivers;
+}
+
+function prependDriver(name) {
+var newDrivers = [...drivers]
+newDrivers.unshift("Broom");
+return newDrivers;
+}
